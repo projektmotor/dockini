2cf0d8f244141cb85e0439a30fbb1c13d8eeec8e
Rombo Kraft <kraft@projektmotor.de>
2019-02-04 20:48:19 +0100

    fix web container waiting for mysql nc check

--- templates/mysql/web/.docker/start-project.sh
+++ templates/mysql/web/.docker/start-project.sh
@@ -3,7 +3,7 @@
 ##############################################################
 
 # wait for mysql service to be available
-until nc -z -v -w30 web 3306 > /dev/null 2>\&1
+until nc -z -v -w30 mysql 3306 > /dev/null 2>\&1
 do
     echo "Waiting for MySQL connection "
     sleep 5
